d41f1f0512547acc3469fd7681f91855c0876b72
krish736 <112505529+krish736@users.noreply.github.com>
2024-07-13 22:59:48 +0530

    1

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,16 @@
-import {BrowserRouter, Routes ,Route } from 'react-router-dom'
-import Home from './pages/Home'
-import Dashboard from './pages/Dashboard'
-import SignIn from './pages/SignIn'
-import SignUp from './pages/SignUp'
-import About from './pages/About'
-import Projects from './pages/Projects'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Dashboard from "./pages/Dashboard";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import About from "./pages/About";
+import Projects from "./pages/Projects";
+import Header from "./components/Header";
 
 const App = () => {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/dashboard" element={<Dashboard />} />
@@ -19,6 +21,6 @@ const App = () => {
       </Routes>
     </BrowserRouter>
   );
-}
+};
 
-export default App
+export default App;
