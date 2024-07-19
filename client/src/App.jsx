29082f7e50fb46a42e69b435dd8c9a1bf52e5541
krish736 <112505529+krish736@users.noreply.github.com>
2024-07-19 23:27:23 +0530

    flowbite set

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,26 +1,14 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
-import Dashboard from "./pages/Dashboard";
-import SignIn from "./pages/SignIn";
-import SignUp from "./pages/SignUp";
-import About from "./pages/About";
-import Projects from "./pages/Projects";
-import Header from "./components/Header";
+import React from 'react'
+import Header from './components/Header'
+import { BrowserRouter, Routes } from 'react-router-dom'
 
-const App = () => {
+export default function App() {
   return (
     <BrowserRouter>
-      <Header />
+      <Header/>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/sign-in" element={<SignIn />} />
-        <Route path="/sign-up" element={<SignUp />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/projects" element={<Projects />} />
+        
       </Routes>
     </BrowserRouter>
-  );
-};
-
-export default App;
+  )
+}
--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,7 +1,13 @@
+import { Navbar } from 'flowbite-react'
+import {Link} from 'react-router-dom'
 import React from 'react'
 
 export default function Header() {
   return (
-    <div>Header</div>
+    <Navbar className='border-b-2'>
+        <Link to='/'>
+        <span className='bg-red-500'>krish</span> Blog
+        </Link>
+    </Navbar>
   )
 }
