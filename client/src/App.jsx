9038f9e3a9bb0cd65d57455f76223cf73ef7f24c
krish736 <112505529+krish736@users.noreply.github.com>
2024-07-20 15:53:58 +0530

    commit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,25 @@
 import React from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import About from './pages/About'
+import Dashboard from './pages/Dashboard'
+import Home from './pages/Home'
+import Projects from './pages/Projects'
+import SignIn from './pages/SignIn'
+import SignUp from './pages/SignUp'
 import Header from './components/Header'
-import { BrowserRouter, Routes } from 'react-router-dom'
 
 export default function App() {
   return (
     <BrowserRouter>
-      <Header/>
+      <Header />
       <Routes>
-        
+        <Route path="/about" element={<About />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/signin" element={<SignIn />} />
+        <Route path="/signup" element={<SignUp />} />
       </Routes>
     </BrowserRouter>
-  )
+  );
 }
