7f58a3ac652ed80b2df5aefb0d60b74df171434f
krish736 <112505529+krish736@users.noreply.github.com>
2024-07-21 23:21:59 +0530

    commit

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,3 +1,28 @@
-export const signup = (req,res)=>{
-    console.log(req.body);
-}
+import User from "../models/users.model.js";
+import bcrypt from "bcrypt";
+
+export const signup = async (req, res) => {
+  const { username, email, password } = req.body;
+
+  if (
+    !username ||
+    !email ||
+    !password ||
+    username === "" ||
+    email === "" ||
+    password === ""
+  ) {
+    res.status(400).json({ error: "All fields are required!" });
+  }
+
+  const hashedPassword = bcrypt.hashSync(password, 10);
+
+  const newUser = new User({ username, email, password: hashedPassword });
+
+  try {
+    await newUser.save();
+    res.json("SignUp succesful");
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
